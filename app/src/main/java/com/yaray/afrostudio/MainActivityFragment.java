395bc402ae649827f4e8742f8e8f8e06b4da210c
Alex Kachanov <mail@alex29.dp.ua>
2025-02-28 14:43:24 +0800

    Enable menu in Fragment

--- app/src/main/java/com/yaray/afrostudio/MainActivityFragment.java
+++ app/src/main/java/com/yaray/afrostudio/MainActivityFragment.java
@@ -4,12 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.fragment.app.Fragment;
 
-/**
- * A placeholder fragment containing a simple view.
- */
 public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {
@@ -19,5 +15,11 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true); // Enable menu in fragment
     }
 }
