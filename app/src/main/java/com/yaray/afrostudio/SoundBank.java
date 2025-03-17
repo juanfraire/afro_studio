5949c30362c1a758ebd6889b196506396335e3f2
Alex Kachanov <mail@alex29.dp.ua>
2025-03-17 21:47:17 +0800

    wip: simplify loops in the PlayRhythmTask for each instrument  (not finished: balet, and djembe offsets)

--- app/src/main/java/com/yaray/afrostudio/SoundBank.java
+++ app/src/main/java/com/yaray/afrostudio/SoundBank.java
@@ -93,6 +93,8 @@ public class SoundBank {
         String[] instruments = {"dun", "ken", "sag"};
         String[] soundTypes = {"bass", "bass_mute", "bass_bell", "bell", "bass_bell_mute"};
 
+        // TODO: interesting, we only use "bell", "bass_bell" and "bass_bell_mute". Can we use the others?
+
         for (String instrument : instruments) {
             for (String soundType : soundTypes) {
                 int resId = getResourceId(context, "snd_" + instrument + "_" + soundType, "raw");
